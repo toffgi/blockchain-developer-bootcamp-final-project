0c6cabf3b8fb0e22ca6108237dd608324fa7bc53
toffgi <toffoletto.g@gmail.com>
2021-12-27 21:29:43 +0100

    Updates

---
+++ precache-manifest.c788cd7137f18edc1a1ebf33d47c3ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d25c3993908f86d16e8",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.c8db3484.chunk.css"
+  },
+  {
+    "revision": "5d25c3993908f86d16e8",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.1688c69f.chunk.js"
+  },
+  {
+    "revision": "07ba0b3a8d9c2087905f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.62014514.js"
+  },
+  {
+    "revision": "3e6ca1c0dbcdca4ef270",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.698cca24.chunk.js"
+  },
+  {
+    "revision": "fd681d4afacedfa227aa57e2245b658f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/media/metamask-logo.fd681d4a.svg"
+  },
+  {
+    "revision": "27cfe69155bc0a5f362a0e7c25ae2f6a",
+    "url": "/blockchain-developer-bootcamp-final-project/static/media/opensea-logo.27cfe691.svg"
+  },
+  {
+    "revision": "035feeb63bafee316913a11183335259",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/blockchain-developer-bootcamp-final-project/precache-manifest.c788cd7137f18edc1a1ebf33d47c3ce0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/blockchain-developer-bootcamp-final-project/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
